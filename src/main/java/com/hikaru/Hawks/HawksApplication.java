4217831b0d8766b5a70b6375c2f48ff2261370e1
sugahikaru <sh09054713265@outlook.jp>
2024-06-03 22:15:14 +0900

    プロジェクトをセットアップ

---
+++ src/main/java/com/hikaru/Hawks/HawksApplication.java
@@ -0,0 +1,13 @@
+package com.hikaru.Hawks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class git HawksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HawksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hikaru/Hawks/HawksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hikaru.Hawks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
